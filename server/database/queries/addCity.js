5c2c2d3a7db4356a5c408f62be6aa099213376a3
ali <ali.7@outlook.com>
2019-08-31 14:09:57 +0300

    add-get-and-add-city-queries

---
+++ server/database/queries/addCity.js
@@ -0,0 +1,14 @@
+const dbConnection = require("../config/connection");
+
+const postData = userData => {
+  const { name, country } = userData;
+  const sql = {
+    text: "INSERT INTO users (name, country) VALUES ($1, $2);",
+    values: [name, country]
+  };
+  return dbConnection.query(sql);
+};
+
+module.exports = {
+  addCity
+};
