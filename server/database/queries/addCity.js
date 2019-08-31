18523721692e1d8c5768340675559180570c1061
ali <ali.7@outlook.com>
2019-08-31 14:25:57 +0300

    fix-addCity-query

--- server/database/queries/addCity.js
+++ server/database/queries/addCity.js
@@ -3,7 +3,7 @@ const dbConnection = require('../config/connection');
 const addCity = userData => {
   const { name, country } = userData;
   const sql = {
-    text: 'INSERT INTO users (name, country) VALUES ($1, $2);',
+    text: 'INSERT INTO city (name, country) VALUES ($1, $2);',
     values: [name, country],
   };
   return dbConnection.query(sql);
