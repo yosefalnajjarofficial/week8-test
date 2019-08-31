276ed7a762e68f6351a0cc0c47ac3094a2fc053b
ali <ali.7@outlook.com>
2019-08-31 17:16:58 +0300

    add-singup-login-pages

--- public/app.js
+++ public/app.js
@@ -1,13 +1,13 @@
-fetch("/cities")
+fetch('/all-cities')
   .then(res => res.json())
   .then(cities => {
-    const table = document.getElementById("cities-table");
+    const table = document.getElementById('cities-table');
     cities.forEach(city => {
-      const row = document.createElement("tr");
-      const name = document.createElement("td");
+      const row = document.createElement('tr');
+      const name = document.createElement('td');
       name.textContent = city.name;
       row.appendChild(name);
-      const country = document.createElement("td");
+      const country = document.createElement('td');
       country.textContent = city.country;
       row.appendChild(country);
       table.appendChild(row);
