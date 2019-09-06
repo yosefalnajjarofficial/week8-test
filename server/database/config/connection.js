833f0aa4daa57e33e907849b4f036e498d013622
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-09-06 21:38:26 +0300

    modified the connection file

--- server/database/config/connection.js
+++ server/database/config/connection.js
@@ -1,13 +1,19 @@
 const { Pool } = require('pg');
 require('env2')('./config.env');
 
-let dbUrl = process.env.DB_URL;
+let dbUrl;
+
+if (process.env.NODE_ENV === 'production') {
+  dbUrl = process.env.DATABASE_URL;
+} else {
+  dbUrl = process.env.DB_URL;
+}
 
 if (!dbUrl) throw new Error('No Database URL!!!');
 
 const options = {
   connectionString: dbUrl,
-  ssl: true,
+  ssl: true
 };
 
 module.exports = new Pool(options);
